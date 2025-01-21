edb2dc553e7721a71331a0aa07fa13bc8cdc3baa
kc-allan <allancheruiyot003@gmail.com>
2025-01-22 00:03:23 +0300

    float to string conversion

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+"""
+Float to string conversion module
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that returns the string representation of a float
+    :param n: float to be converted
+    :return: string representation of n
+    """
+    return f'{n}'
